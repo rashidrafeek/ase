7c94a21737f872fea9e02ddda0f99b5dd6d60f7a
Irina Lebedeva <liv_ira@hotmail.com>
2024-05-09 12:00:08 +0200

    Changes

--- ase/test/calculator/siesta/test_write_input.py
+++ ase/test/calculator/siesta/test_write_input.py
@@ -101,7 +101,8 @@ def test_species(factory, atoms_ch4):
             Species(
                 symbol='H',
                 tag=1,
-                basis_set='SZ')])
+                basis_set='SZ',
+                pseudopotential='lda/H.psml')])
     species, numbers = siesta.species(atoms_ch4)
     assert all(numbers == np.array([1, 2, 2, 3, 2]))
 
@@ -110,9 +111,9 @@ def test_species(factory, atoms_ch4):
     with open('test_label.fdf', encoding='utf-8') as fd:
         lines = fd.readlines()
     lines = [line.split() for line in lines]
-    assert ['1', '6', 'C.lda.1'] in lines
-    assert ['2', '1', 'H.lda.2'] in lines
-    assert ['3', '1', 'H.lda.3'] in lines
+    assert ['1', '6', 'C.lda.1', ''] in lines
+    assert ['2', '1', 'H.lda.2', ''] in lines
+    assert ['3', '1', 'H.lda.3', 'lda/H.psml'] in lines
     assert ['C.lda.1', 'DZP'] in lines
     assert ['H.lda.2', 'DZP'] in lines
     assert ['H.lda.3', 'SZ'] in lines
