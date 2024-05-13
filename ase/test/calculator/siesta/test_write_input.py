3ffb227ad343b0d848f08680014fb17f71346e21
Irina Lebedeva <liv_ira@hotmail.com>
2024-05-13 11:15:33 +0200

    Changes

--- ase/test/calculator/siesta/test_write_input.py
+++ ase/test/calculator/siesta/test_write_input.py
@@ -113,7 +113,6 @@ def test_species(factory, atoms_ch4):
         lines = fd.readlines()
 
     lines = [line.split() for line in lines]
-    print(lines)
     assert ['1', '6', 'C.lda.1'] in lines
     assert ['2', '1', 'H.lda.2'] in lines
     assert ['3', '1', 'H.3', 'H.psml'] in lines
