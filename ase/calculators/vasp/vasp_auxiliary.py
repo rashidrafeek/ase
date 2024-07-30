4b60cbdc48fee3a682458c39e06a08e0b7fc9085
Chengcheng Xiao <iconxicon@me.com>
2024-07-30 19:08:45 +0000

    Update file vasp_auxiliary.py

--- ase/calculators/vasp/vasp_auxiliary.py
+++ ase/calculators/vasp/vasp_auxiliary.py
@@ -208,8 +208,7 @@ class VaspChargeDensity:
                     continue  # Write only the last image for CHGCAR
                 aiv.write_vasp(fd,
                                self.atoms[ii],
-                               direct=True,
-                               long_format=False)
+                               direct=True)
                 fd.write('\n')
                 for dim in chg.shape:
                     fd.write(' %4i' % dim)
