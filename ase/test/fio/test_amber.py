15a6fb60c6473a01299d1617b16c71732b87f0cc
Ask Hjorth Larsen <asklarsen@gmail.com>
2024-08-09 15:48:44 +0200

    test both with and without velocities

--- ase/test/fio/test_amber.py
+++ ase/test/fio/test_amber.py
@@ -1,21 +1,26 @@
 import pytest
 
+import numpy as np
 from ase.build import bulk
 from ase.calculators.calculator import compare_atoms
 from ase.io.amber import read_amber_coordinates, write_amber_coordinates
 
 
-def test_io_amber_coordinates():
+@pytest.mark.parametrize('with_velocities', [False, True])
+def test_io_amber_coordinates(with_velocities):
     atoms = bulk('Au', orthorhombic=True)
     filename = 'amber.netcdf'
-    write_amber_coordinates(atoms, filename)
 
-    print(atoms.cell)
+    if with_velocities:
+        atoms.set_velocities(np.random.default_rng(42).random((len(atoms), 3)))
+
+    write_amber_coordinates(atoms, filename)
     atoms2 = read_amber_coordinates(filename)
-    print(atoms2.cell)
+
     # The format does not save the species so they revert to 'X'
     assert all(atoms2.symbols == 'X')
     assert compare_atoms(atoms, atoms2) == ['numbers']
+    assert atoms.get_velocities() == pytest.approx(atoms2.get_velocities())
 
 
 def test_cannot_write_nonorthorhombic():
