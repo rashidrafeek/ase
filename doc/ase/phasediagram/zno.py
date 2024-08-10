b7f095e5ebe2ca0af6dbc6965cba140b9c9ba9ed
Ask Hjorth Larsen <asklarsen@gmail.com>
2024-08-10 16:59:17 +0200

    update example

--- doc/ase/phasediagram/zno.py
+++ doc/ase/phasediagram/zno.py
@@ -9,11 +9,10 @@ energy, phase = pbx.get_pourbaix_energy(1.0, 9.0, verbose=True)
 print(type(phase))
 print(phase.get_free_energy(1.0, 9.0))
 pbx.get_pourbaix_energy(0.0, 10.0, verbose=True)
-Urange = [-2, 2]
-pHrange = [0, 14]
-pbx.plot(
-    Urange=Urange,
-    pHrange=pHrange,
+
+diagram = pbx.diagram(U=np.linspace(-2, 2, 100), pH=np.linspace(0, 14, 100))
+
+diagram.plot(
     show=False,
     include_text=True,
     savefig='zno.png'
