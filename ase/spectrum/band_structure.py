a112a14a726883c997e07d18d98bd185b18a48c3
Jakob Schiotz <schiotz@fysik.dtu.dk>
2024-09-11 15:34:47 +0200

    Fix BandStructurePlot.plot_with_colors() so it actually work.

--- ase/spectrum/band_structure.py
+++ ase/spectrum/band_structure.py
@@ -186,6 +186,7 @@ class BandStructurePlot:
             e_skn = self.bs.energies
         else:
             e_skn = self.bs.energies[spin, np.newaxis]
+        print('E_SKN:', e_skn.shape)
 
         if colors is None:
             if len(e_skn) == 1:
@@ -216,8 +217,8 @@ class BandStructurePlot:
 
         return ax
 
-    def plot_with_colors(self, ax=None, emin=-10, emax=5, filename=None,
-                         show=False, energies=None, colors=None,
+    def plot_with_colors(self, ax=None, spin=None, emin=-10, emax=5, filename=None,
+                         show=False, colors=None,
                          ylabel=None, clabel='$s_z$', cmin=-1.0, cmax=1.0,
                          sortcolors=False, loc=None, s=2):
         """Plot band-structure with colors."""
@@ -227,18 +228,27 @@ class BandStructurePlot:
         if self.ax is None:
             ax = self.prepare_plot(ax, emin, emax, ylabel)
 
+        if spin is None:
+            energies = self.bs.energies
+        else:
+            energies = self.bs.energies[spin, np.newaxis]
+
         shape = energies.shape
-        xcoords = np.vstack([self.xcoords] * shape[1])
+        xcoords = np.zeros(shape)
+        #xcoords = np.stack([self.xcoords] * shape[1], axis=1)
+        xcoords += self.xcoords[np.newaxis, :, np.newaxis]
+        print('XCOORDS:', xcoords.shape)
         if sortcolors:
             perm = colors.argsort(axis=None)
             energies = energies.ravel()[perm].reshape(shape)
             colors = colors.ravel()[perm].reshape(shape)
             xcoords = xcoords.ravel()[perm].reshape(shape)
 
-        for e_k, c_k, x_k in zip(energies, colors, xcoords):
-            things = ax.scatter(x_k, e_k, c=c_k, s=s,
-                                vmin=cmin, vmax=cmax)
-
+        #for e_k, c_k, x_k in zip(energies, colors, xcoords):
+        #    things = ax.scatter(x_k, e_k, c=c_k, s=s,
+        #                        vmin=cmin, vmax=cmax)
+        things = ax.scatter(xcoords, energies, c=colors, s=s,
+                            vmin=cmin, vmax=cmax)
         cbar = plt.colorbar(things)
         cbar.set_label(clabel)
 
