9c78e3c0b8de03b1bdcc6fde72cba8c9d9223235
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2024-10-11 23:24:41 +0200

    Separate out `parse_poscar_scaling_factor`

--- ase/io/vasp.py
+++ ase/io/vasp.py
@@ -25,6 +25,22 @@ __all__ = [
 ]
 
 
+def parse_poscar_scaling_factor(line: str) -> np.ndarray:
+    """Parse scaling factor(s) in the second line in POSCAR/CONTCAR.
+
+    This can also be one negative number or three positive numbers.
+
+    https://www.vasp.at/wiki/index.php/POSCAR#Full_format_specification
+
+    """
+    scale = np.array(line.split()[:3], dtype=float)
+    if len(scale) not in [1, 3]:
+        raise RuntimeError('The number of scaling factors must be 1 or 3.')
+    if len(scale) == 3 and np.any(scale < 0.0):
+        raise RuntimeError('All three scaling factors must be positive.')
+    return scale
+
+
 def get_atomtypes(fname):
     """Given a file name, get the atomic symbols.
 
@@ -139,21 +155,14 @@ def read_vasp(filename='CONTCAR'):
     # file.
     line1 = fd.readline()
 
-    # Scaling factor
-    # This can also be one negative number or three positive numbers.
-    # https://www.vasp.at/wiki/index.php/POSCAR#Full_format_specification
-    scale = np.array(fd.readline().split()[:3], dtype=float)
-    if len(scale) not in [1, 3]:
-        raise RuntimeError('The number of scaling factors must be 1 or 3.')
-    if len(scale) == 3 and np.any(scale < 0.0):
-        raise RuntimeError('All three scaling factors must be positive.')
+    scale = parse_poscar_scaling_factor(fd.readline())
 
     # Now the lattice vectors
     cell = np.array([fd.readline().split()[:3] for _ in range(3)], dtype=float)
     # Negative scaling factor corresponds to the cell volume.
     if scale[0] < 0.0:
         scale = np.cbrt(-1.0 * scale / np.linalg.det(cell))
-    cell *= scale
+    cell *= scale  # This works for both one and three scaling factors.
 
     # Number of atoms. Again this must be in the same order as
     # in the first line
