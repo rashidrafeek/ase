b50d186b19b42c6f2d4c453a933e3ca75b78d5d1
Rohit Goswami <rog32@hi.is>
2024-12-13 15:22:23 +0000

    MAINT: Pass lint check with ruff format

--- ase/test/test_nwchem_writer.py
+++ ase/test/test_nwchem_writer.py
@@ -15,38 +15,34 @@ def atomic_configuration():
 
 @pytest.fixture()
 def calculator_parameters():
-    params = dict(memory='1024 mb',
-                  dft=dict(xc='b3lyp',
-                           mult=1,
-                           maxiter=300),
-                  basis='6-311G*')
+    params = dict(
+        memory='1024 mb',
+        dft=dict(xc='b3lyp', mult=1, maxiter=300),
+        basis='6-311G*',
+    )
     return params
 
 
 def test_echo(atomic_configuration, calculator_parameters, tmp_path):
-    with (tmp_path / "nwchem.in").open("w") as fd:
+    with (tmp_path / 'nwchem.in').open('w') as fd:
         write_nwchem_in(
-            fd,
-            atomic_configuration,
-            echo=False,
-            **calculator_parameters)
-    content = (tmp_path / "nwchem.in").read_text().splitlines()
+            fd, atomic_configuration, echo=False, **calculator_parameters
+        )
+    content = (tmp_path / 'nwchem.in').read_text().splitlines()
     assert 'echo' not in content
 
-    with (tmp_path / "nwchem.in").open("w") as fd:
+    with (tmp_path / 'nwchem.in').open('w') as fd:
         write_nwchem_in(
-            fd,
-            atomic_configuration,
-            echo=True,
-            **calculator_parameters)
-    content = (tmp_path / "nwchem.in").read_text().splitlines()
+            fd, atomic_configuration, echo=True, **calculator_parameters
+        )
+    content = (tmp_path / 'nwchem.in').read_text().splitlines()
     assert 'echo' in content
 
 
 def test_params(atomic_configuration, calculator_parameters, tmp_path):
-    with (tmp_path / "nwchem.in").open("w") as fd:
+    with (tmp_path / 'nwchem.in').open('w') as fd:
         write_nwchem_in(fd, atomic_configuration, **calculator_parameters)
-    content = (tmp_path / "nwchem.in").read_text().splitlines()
+    content = (tmp_path / 'nwchem.in').read_text().splitlines()
 
     for key, value in calculator_parameters.items():
         for line in content:
@@ -75,23 +71,27 @@ def test_write_nwchem_in_set_params(
     in the 'set' parameter, ensuring correct section order.
     Closes #1578
     """
-    atoms = Atoms("C2H6")
+    atoms = Atoms('C2H6')
     cparams = calculator_parameters
-    cparams["set"] = {"geom:dont_verify": True}
-    with (tmp_path / "nwchem.in").open("w") as fd:
+    cparams['set'] = {'geom:dont_verify': True}
+    with (tmp_path / 'nwchem.in').open('w') as fd:
         write_nwchem_in(fd, atomic_configuration, echo=False, **cparams)
-    content = (tmp_path / "nwchem.in").read_text().splitlines()
+    content = (tmp_path / 'nwchem.in').read_text().splitlines()
     # 'set geom:dont_verify .true.' must appear before 'geometry'
     set_line_index = next(
         (
             i
             for i, line in enumerate(content)
-            if line.strip() == "set geom:dont_verify .true."
+            if line.strip() == 'set geom:dont_verify .true.'
         ),
         None,
     )
     geometry_line_index = next(
-        (i for i, line in enumerate(content) if line.strip().startswith("geometry")),
+        (
+            i
+            for i, line in enumerate(content)
+            if line.strip().startswith('geometry')
+        ),
         None,
     )
 
